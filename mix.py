6c8aba185ea5ede5e869dff3a9be6119620c5fc7
Nizan Mandelblit <nizanm91@gmail.com>
2021-06-16 00:15:48 +0300

    config mix.py and reciever.py

--- mix.py
+++ mix.py
@@ -9,8 +9,20 @@ import hashlib
 def main():
     print("hi from mix")
     Y = "sk" + sys.argv[1] + ".pem"
+    port= sys.argv[2]
+
     skfile = open(Y, "r")
-    
+    skfile.close()
+
+    s = socket.socket()  # Create a socket object
+    host = socket.gethostname()  # Get local machine name
+    s.bind((host, port))  # Bind to the port
+    s.listen()  # Now wait for client connection.
+    while True:
+        c, addr = s.accept()  # Establish connection with client.
+        print('Got connection from', addr)
+        print(c.recv(1024))
+        c.close()  # Close the connection
 
 if __name__ == '__main__':
     main()
