ea6c9b1668568cfe4bd584c39a2d5eb1a65b0c1a
EldadHorvitz <eldad373@gmail.com>
2021-06-16 00:38:28 +0300

    key load in mix

--- mix.py
+++ mix.py
@@ -1,16 +1,32 @@
-# nizan mandelblit, 313485468, fullname 2, id 2
+# nizan mandelblit, 313485468, eldad horvitz, 314964438
 import socket, os, datetime, random, sys
 import hashlib
-
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 
 
 
 def main():
     print("hi from mix")
-    Y = "sk" + sys.argv[1] + ".pem"
-    skfile = open(Y, "r")
-    
+    # load sk
+    key = "sk" + sys.argv[1] + ".pem"
+    skfile = open(key, "rb")
+    private_key = serialization.load_pem_private_key(skfile.read(), password=None)
+    # get ciphertext
+    ciphertext = b"hh"
+    # decrypt the ciphertext
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
 
 if __name__ == '__main__':
     main()
