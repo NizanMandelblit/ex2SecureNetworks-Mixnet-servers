731d59a7d096fddca8a93ececdc6a7fb345bf6c7
EldadHorvitz <eldad373@gmail.com>
2021-06-16 14:43:32 +0300

    send massage from mix

--- mix.py
+++ mix.py
@@ -22,7 +22,12 @@ def main():
         print('Got connection from', addr)
         decryptedMsg = private_key.decrypt(c.recv(1024), padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                                                       algorithm=hashes.SHA256(), label=None))
-        socket.inet_ntoa(decryptedMsg[0:4])
+        ipSend = socket.inet_ntoa(decryptedMsg[0:4])
+        portSend = socket.inet_ntoa(decryptedMsg[4:6])
+        send = socket.socket()  # Create a socket object
+        send.connect((ipSend, int(portSend)))
+        send.sendall(decryptedMsg[6:])
+        s.close()  # Close the socket when done
 
 
         print(decryptedMsg)
